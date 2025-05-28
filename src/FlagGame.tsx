e5629766f20cd51314b6a2cfa096666e3522102a
Callum <callumbradding@gmail.com>
2025-05-28 17:20:21 +1200

    Removed unused variable

--- src/FlagGame.tsx
+++ src/FlagGame.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { flags as allFlags } from "./data/flags";
 import type { Flag } from "./data/flags";
 
