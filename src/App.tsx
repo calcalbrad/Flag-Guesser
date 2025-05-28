27af5e0023504aa7cb3d5a49afc11bb7cc78b16b
Callum <callumbradding@gmail.com>
2025-05-28 15:58:43 +1200

    Added to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import FlagGame from "./FlagGame"
+
 function App() {
   return (
     <div style={{ padding: '2rem', textAlign: 'center' }}>
       <h1>Flag Guessing Game</h1>
       <p>Let’s test your world flag knowledge!</p>
+
+      <FlagGame />
     </div>
   )
 }
